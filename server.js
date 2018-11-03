290c121bee68a1f3f3c5b8238fcbd79f5be15bd4
moldingtofu <jeremykchang@gmail.com>
2018-11-03 05:59:25 -0400

    uhh lol

--- server.js
+++ server.js
@@ -1,35 +1,8 @@
-//const SpotifyStrategy = require('passport-spotify').Strategy;
-//
-//passport.use (
-//  new SpotifyStrategy (
-//    {
-//      clientID: client_id,
-//      clientSecret: client_secret,
-//      callbackURL: 'http://localhost:8888/auth/spotify/callback'
-//    },
-//    function(accessToken, refreshToken, expires_in, profile, done) {
-//      User.findOrCreate({ spotifyId: profile.id },
-//        return done(err, user);
-//      });
-//    }
-//  )
-//);
-//
-//app.get('/auth/spotify', passport.authenticate('spotify'), function(req, res) {
-//
-//});
-//
-//app.get(
-//  '/auth/spotify/callback',
-//  passport.authenticate('spotify', { failureRedirect: '/login' }),
-//  function(req, res) {
-//    res.redirect('/');
-//  }
-//);
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const passport = require('passport');
 
 require('dotenv').config();
 
@@ -50,6 +23,32 @@ app.get('/', function(req, res) {
   res.send('Hello');
 });
 
+passport.use (
+  new SpotifyStrategy (
+    {
+      clientID: process.env.CLIENTID,
+      clientSecret: process.env.CLIENTSECRET,
+      callbackURL: 'http://localhost:8080/auth/spotify/callback'
+    },
+    function(accessToken, refreshToken, expires_in, profile, done) {
+      User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
+        return done(err, user);
+      });
+    }
+  )
+);
+
+app.get('/auth/spotify', passport.authenticate('spotify'), function(req, res) {
+  //redirect to spotify authentication
+});
+
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
 
 server = app.listen(8080);
 console.log('Express listening port: 8080');
